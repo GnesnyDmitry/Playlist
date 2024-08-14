a6425b8283705bcfa4962ee8c48aedd9ed424d83
GnesnyDmitry <gnensydmitry@gmail.com>
2024-08-14 20:33:31 +0400

    creat 2 screen and navigation

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_root)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnSearch = findViewById<Button>(R.id.activity_root_btn_search)
+        val btnMedia = findViewById<Button>(R.id.activity_root_btn_media)
+        val btnSettings = findViewById<Button>(R.id.activity_root_btn_settings)
+
+        btnSearch.setOnClickListener {
+            val goSearchActivity = Intent(this, SearchActivity::class.java)
+            startActivity(goSearchActivity)
+        }
+
+        btnMedia.setOnClickListener {
+            val goMediaActivity = Intent(this, MediaActivity::class.java)
+            startActivity(goMediaActivity)
+        }
+
+        btnSettings.setOnClickListener {
+            val goSettingsActivity = Intent(this, SettingsActivity::class.java)
+            startActivity(goSettingsActivity)
         }
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,31 @@
+package com.example.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings_root)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toolbarSettings = findViewById<Toolbar>(R.id.settings_root_toolbar)
+
+        toolbarSettings.setNavigationOnClickListener {
+            val goActivityRoot = Intent(this, MainActivity::class.java)
+            startActivity(goActivityRoot)
+        }
+    }
+}
