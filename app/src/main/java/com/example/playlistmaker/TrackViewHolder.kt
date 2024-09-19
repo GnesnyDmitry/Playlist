d76187b4774fbe264a3961f536259e3ae1f529b2
GnesnyDmitry <gnensydmitry@gmail.com>
2024-09-19 16:40:25 +0400

    dev_sprint_11 add iTunesApi

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -1,16 +1,15 @@
 package com.example.playlistmaker
 
 import android.view.LayoutInflater
-import android.view.RoundedCorner
-import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.model.Model
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class TrackViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
@@ -24,13 +23,17 @@ class TrackViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     fun bind(track: Track) {
         trackName.text = track.trackName
         artistName.text = track.artistName
-        trackTime.text = track.trackTime
+        trackTime.text = formatTrackTime(track.trackTimeMillis)
+
         Glide
             .with(itemView.context)
-            .load(track.artworkUrl)
+            .load(track.artworkUrl100)
             .placeholder(R.drawable.placeholder)
             .transform(RoundedCorners(4))
             .into(pictureTrack)
+    }
 
+    private fun formatTrackTime(trackTimeMillis: Int): String {
+        return SimpleDateFormat("mm:ss", Locale.getDefault()).format(trackTimeMillis)
     }
 }
