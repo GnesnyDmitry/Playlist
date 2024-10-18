6c409f0e76bfcc1ee2d865c10822c7a86712b87f
GnesnyDmitry <gnensydmitry@gmail.com>
2024-10-18 17:33:10 +0400

    dev_sprint_13 added audio player screen and transistion on it

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -23,7 +23,6 @@ class TrackViewHolder(parent: ViewGroup,
     private val trackTime: TextView = itemView.findViewById(R.id.time_track)
 
     fun bind(track: Track) {
-        println("qqq bind")
         trackName.text = track.trackName
         artistName.text = track.artistName
         trackTime.text = formatTrackTime(track.trackTimeMillis)
